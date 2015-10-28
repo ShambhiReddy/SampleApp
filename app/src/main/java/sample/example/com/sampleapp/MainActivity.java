e64bfb1d221b19a9aae8128e61f9d581dc8191d1
ShambhiReddy <shambhi.koppula@mtuity.com>
2015-10-28 12:15:04 +0530

    hi testing

---
+++ app/src/main/java/sample/example/com/sampleapp/MainActivity.java
@@ -0,0 +1,32 @@
+package sample.example.com.sampleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    Button button1;
+int duration=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1=(Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        Toast.makeText(this,"Hai from first app",Toast.LENGTH_SHORT).show();
+    }
+}
